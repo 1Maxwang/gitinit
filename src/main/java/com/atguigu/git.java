382fc76cc613002563cc8ee72be6236e2fb70b97
MaxWong <15379194+maxwong123@user.noreply.gitee.com>
2025-01-18 21:10:37 +0800

    master commit2

--- src/main/java/com/atguigu/git.java
+++ src/main/java/com/atguigu/git.java
@@ -4,6 +4,6 @@ public class git {
     public static void main(String[] args) {
         System.out.println("hello gttywdguwgdgdugww");
         System.out.println("hello gttywdguwgdgdugww1111");
-        System.out.println("hello gt11");
+        System.out.println("hello master");
     }
 }

684f211885665c2d20f6f6b980e3cb4f53fa1db6
MaxWong <15379194+maxwong123@user.noreply.gitee.com>
2025-01-18 19:34:35 +0800

    second commit

--- src/main/java/com/atguigu/git.java
+++ src/main/java/com/atguigu/git.java
@@ -2,6 +2,6 @@ package com.atguigu;
 
 public class git {
     public static void main(String[] args) {
-        System.out.println("hello gtt");
+        System.out.println("hello gttywdguwgdgdugww");
     }
 }

f3c0264e163dc6726bfbce5ad1a0e82ea11b46fd
1Maxwang <63296862+1Maxwang@users.noreply.github.com>
2025-01-19 12:13:56 +0800

    Update commit

--- src/main/java/com/atguigu/git.java
+++ src/main/java/com/atguigu/git.java
@@ -2,9 +2,6 @@ package com.atguigu;
 
 public class git {
     public static void main(String[] args) {
-        System.out.println("hello gttywdguwgdgdugww");
-        System.out.println("hello gttywdguwgdgdugww1111");
-        System.out.println("hello master");
-        System.out.println("pushlo hot");
+        System.out.println("hello world");
     }
 }
